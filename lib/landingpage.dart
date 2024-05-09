8f0f7e0343a68ef5ec82029b9cb3cc731736f69d
abhaybissa31 <theabhaybissa@gmail.com>
2024-05-09 18:26:28 +0530

    Home page done for the app

---
+++ lib/landingpage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  LandingPage({super.key});
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset("assests/images/quiz-logo.png", width: 300,color: const Color.fromARGB(169, 255, 255, 255),),
+          const SizedBox(height: 60,),
+          const Text("Learning New Tech is fun", style: TextStyle(color: Color.fromARGB(255, 239, 184, 249), fontSize: 24,),),
+          const SizedBox(height:20,),
+          OutlinedButton.icon(onPressed: () {},style: OutlinedButton.styleFrom(foregroundColor:Colors.white) ,label:const Text("Start Quiz"),icon:const Icon(Icons.arrow_forward_ios_outlined),)
+        ],
+      ),
+    );
+  }
+}
